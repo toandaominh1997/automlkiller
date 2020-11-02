0833ef6b704344c794a35c06bee8a0a03fbc09cf
toandm2 <toandm2@onemount.com>
2020-11-02 11:39:33 +0700

    Fix setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 setup(
     name="autotonne",
-    version="0.0.1",
+    version="0.0.2",
     description="Auto machine learning, deep learning library in Python.",
     long_description=readme(),
     long_description_content_type="text/markdown",
