760acc68c7583f43b1ebe142e72fe9991248ceb0
toandm2 <toandm2@onemount.com>
2020-11-02 11:31:25 +0700

    publish distribution

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 def readme():
     with open('README.md') as f:
@@ -9,20 +9,20 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 setup(
     name="autotonne",
-    version="0.1.1",
+    version="0.0.1",
     description="Auto machine learning, deep learning library in Python.",
     long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/toandaominh1997/autotonne",
-    author="Tonne",
+    author="toandaominh1997",
     author_email="toandaominh1997@gmail.com",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     packages=["autotonne"],
     include_package_data=True,
     install_requires=required
